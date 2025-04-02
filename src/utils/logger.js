10fe821d0e03784da5b36afb548e0f1058f92c01
Emily Williams <ceo@projectcosign.com>
2025-04-02 16:29:05 +0000

    changes to logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,4 +1,18 @@
-export default {
-    info: console.log,
-    error: console.error,
+const logger = {
+    info: (...args) => {
+        console.log('[INFO]', ...args)
+    },
+    warn: (...args) => {
+        console.warn('[WARN]', ...args)
+    },
+    error: (...args) => {
+        console.error('[ERROR]', ...args)
+    },
+    debug: (...args) => {
+        if (process.env.NODE_ENV !== 'production') {
+            console.debug('[DEBUG]', ...args)
+        }
+    },
 }
+
+export default logger
